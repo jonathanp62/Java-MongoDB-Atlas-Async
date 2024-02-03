74f3becce9f3fb920127865e421d269750c6aef9
Jonathan Parker <jonathanp62@gmail.com>
2024-02-03 13:36:08 -0500

    Updated the component commentary throughout

--- src/main/java/net/jmp/demo/mongodb/atlas/async/PrintDocumentSubscriber.java
+++ src/main/java/net/jmp/demo/mongodb/atlas/async/PrintDocumentSubscriber.java
@@ -3,9 +3,6 @@ package net.jmp.demo.mongodb.atlas.async;
 /*
  * (#)PrintDocumentSubscriber.java  0.1.0   12/19/2023
  *
- * Copyright (c) Jonathan M. Parker
- * All Rights Reserved.
- *
  * For the purposes of learning and developing this
  * demonstration program, this class borrows heavily
  * from and acknowledges the following cited source
@@ -16,6 +13,28 @@ package net.jmp.demo.mongodb.atlas.async;
  * @author    Jonathan Parker
  * @version   0.1.0
  * @since     0.1.0
+ *
+ * MIT License
+ *
+ * Copyright (c) 2023 Jonathan M. Parker
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 import org.bson.Document;
